60302574c2cd84c0b21cfd6a5946786218ead755
André Vasconcelos <andytsunami@hotmail.com>
2019-02-20 14:40:49 -0300

    Transfomando a controller de foto em lista e iterando nela a partir da view

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,6 +1,20 @@
 angular.module("alurapic").controller("FotosController",function($scope){
-    $scope.foto = {
-        titulo: "Leão",
-        url: "http://www.fundosanimais.com/Minis/leoes.jpg"
-    };
+    $scope.fotos = [
+        {
+            titulo: "Leão 1",
+            url: "http://www.fundosanimais.com/Minis/leoes.jpg"
+        },
+        {
+            titulo: "Leão 2",
+            url: "http://www.fundosanimais.com/Minis/leoes.jpg"
+        },
+        {
+            titulo: "Leão 3",
+            url: "http://www.fundosanimais.com/Minis/leoes.jpg"
+        },
+        {
+            titulo: "Leão 4",
+            url: "http://www.fundosanimais.com/Minis/leoes.jpg"
+        }
+    ]
 });
