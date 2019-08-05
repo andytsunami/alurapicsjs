b65c48576b568adfaf9a4c77af4e1b75dde5b845
André Vasconcelos <andytsunami@hotmail.com>
2019-08-05 19:11:01 -0300

    Implementando recuso em todas as paginas

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,19 +1,37 @@
-angular.module("alurapic").controller("FotoController",function($scope,$http){
+angular.module("alurapic").controller("FotoController",function($scope,recursoFoto,$routeParams){
     $scope.foto = {};
     $scope.mensagem = "";
+
+    if($routeParams.fotoId){
+        recursoFoto.get({fotoId: $routeParams.fotoId}, function(foto){
+            $scope.foto = foto;
+        }, function(erro){
+            console.log(erro);
+            $scope.mensagem = "Não foi possível obter a foto";
+        })
+    }
 
     $scope.submeter = function(){
 
         if($scope.formulario.$valid){
 
-            $http.post("/v1/fotos",$scope.foto).success(function(){
-                $scope.foto = {};
-                $scope.mensagem = "Adicionado com sucesso";
-                $scope.formulario.$setPristine();
-            })
-            .error(function(erro){
-                $scope.mensagem = "Não foi possivel cadastrar a foto.";
-            })
+            if($routeParams.fotoId){
+                recursoFoto.update({fotoId: $scope.foto._id}, 
+                    $scope.foto, function(){
+                        $scope.mensagem = "Foto alterada com sucesso";
+                    }, function(erro){
+                        console.log(erro);
+                        $scope.mensagem = "Não foi possível alterar";
+                    })
+            } else {
+                recursoFoto.save($scope.foto, function(){
+                    $scope.foto = {};
+                    $scope.mensagem = "Foto cadastrada com sucesso";   
+                }, function(erro){
+                    console.log(erro);
+                    $scope.mensagem = "Não foi possível cadastrar a foto";
+                })
+            }           
         }
     }
 });
--- public/js/main.js
+++ public/js/main.js
@@ -11,7 +11,13 @@ angular.module("alurapic",["minhasDiretivas","ngAnimate","ngRoute","meusServicos
         templateUrl: "partials/foto.html",
         controller: "FotoController"
     
-    });
+    })
+    .when("/fotos/edit/:fotoId",{
+        templateUrl: "partials/foto.html",
+        controller: "FotoController"
+    })
+
+
 
     $routeProvider.otherwise({redirectTo: "/fotos"})
 });
