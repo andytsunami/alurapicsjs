dc88f37052ef0540f5a1a0f6ddffd24b88cb229d
André Vasconcelos <andytsunami@hotmail.com>
2019-08-30 18:38:05 -0300

    Utilizando promise para salvar/editar fotos

--- public/js/services/meus-servicos.js
+++ public/js/services/meus-servicos.js
@@ -6,3 +6,39 @@ angular.module("meusServicos",["ngResource"])
         }
     });
 })
+.factory("cadastroDeFotos", function(recursoFoto,$q){
+    var service = {};
+    service.cadastrar = function(foto){
+        return $q(function(resolve, reject){
+            if(foto._id){
+                recursoFoto.update({fotoId: foto._id}, foto, function(){
+                    resolve({
+                        mensagem: "Foto " + foto.titulo + " atualizada com sucesso",
+                        inclusao: false
+                    });
+                }, function(erro){
+                    console.log(erro);
+                    reject({
+                        mensagem: "Não foi possivel atualizar a foto " + foto.titulo
+                    })
+                }
+                )
+            } else {
+                recursoFoto.save(foto,function(){
+                    resolve({
+                        mensagem: "Foto incluida com sucesso",
+                        inclusao: true
+                    });
+                }, function(erro){
+                    console.log(erro);
+                    reject({
+                        mensagem: "Não foi possivel incluir a foto " + foto.titulo
+                    })
+
+                })
+            }
+
+        })
+    }
+    return service;
+});
