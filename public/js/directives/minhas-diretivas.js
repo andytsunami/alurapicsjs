58e721d5e9382262354a708179675a004c0fe9f9
André Vasconcelos <andre.bruno@resourceit.com>
2019-09-24 14:59:45 -0300

    Listando titulos com MAP

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -47,4 +47,17 @@ angular.module("minhasDiretivas",[]).directive("meuPainel",function(){
     };
 
     return ddo;
+})
+.directive("meusTitulos", function(){
+    var ddo = {};
+    ddo.restrict = "E";
+    ddo.template = "<ul><li ng-repeat='titulo in titulos'>{{titulo}}</li></ul>";
+    ddo.controller = function($scope,recursoFoto){
+        recursoFoto.query(function(fotos) {
+            $scope.titulos = fotos.map(function(foto) {
+                return foto.titulo;
+            });    
+        });
+    };
+    return ddo;
 });
